73d816f735e912df1de5cec77f086faa3339c61c
unknown <fizz.zehra14@gmail.com>
2021-07-07 16:11:25 -0500

    creating dynamic readme

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const utils = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //ask for project title
+    {
+        type: 'input',
+        name : 'title',
+        message: 'What is your project title?'
+    }
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your project description'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
